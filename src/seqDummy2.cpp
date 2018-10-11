651b4266b8062caddadad59c8899868392f19af8
Konstantinos Triantafyllou <ko.trian@gmail.com>
2018-10-12 01:06:56 +0200

    Optimize sequential version

--- src/seqDummy2.cpp
+++ src/seqDummy2.cpp
@@ -7,66 +7,68 @@
 
 using namespace std;
 
-const int N=5;      // the number of particles
-const float g=1 ;     // gravitational constant
-const float epsilon=0.001;
+const int N = 5;      // the number of particles
+const float g = 1 ;     // gravitational constant
+const float epsilon = 0.001;
+const float epsilon2 = epsilon * epsilon;
 
 // min ksexaseis na arxikopoihseis mazes kai taxitites
 
-void InitialPositions(float *x,float *y)
+void InitialPositions(float (*r)[2])
 {
     srand(time(NULL));
     for (int i=0; i<N; i++)
     {
-        x[i] = ((double) rand()) / RAND_MAX;
-        y[i] = ((double) rand()) / RAND_MAX;
+        r[i][0] = ((double) rand()) / RAND_MAX;
+        r[i][1] = ((double) rand()) / RAND_MAX;
     }
 }
-void ComputeF(int j,float *x,float *y, float *m,float *ax)
+
+void ComputeF(int j, float (*r)[2], float *m, float (*a)[2])
 {
     for (int i=j+1; i<N; i++)
     {
-        if(i!=j)
-        {
-            float d=sqrt((x[i]-x[j])*(x[i]-x[j])+(y[i]-y[j])*(y[i]-y[j]));
-                float denom = sqrt((d*d+epsilon*epsilon)*(d*d+epsilon*epsilon)*(d*d+epsilon*epsilon));
-                float force = -g*m[j]*m[i]*(x[j]-x[i])/denom;
-                ax[j]+=force/m[j];
-                ax[i]-=force/m[i];
-              //  sumF+=F;
-        }
+        float rij[2];
+        rij[0] = r[i][0] - r[j][0];
+        rij[1] = r[i][1] - r[j][1];
+        float r2 = rij[0] * rij[0] + rij[1] * rij[1];
+        float denom = (r2+epsilon2) * sqrt(r2+epsilon2);
+        float fi = -g * m[j] / denom;
+        float fj = -g * m[i] / denom;
+        a[i][0] += fi * rij[0];
+        a[i][1] += fi * rij[1];
+        a[j][0] -= fj * rij[0];
+        a[j][1] -= fj * rij[1];
     }
 }
 
-
 int main(int argc,char** argv)
 {
-    float x[N],y[N],ux[N],uy[N],m[N],ax[N],ay[N];
+    float r[N][2], u[N][2], a[N][2], m[N];
     ofstream myfile;
     myfile.open ("output.dat");
 
-    InitialPositions(x,y);
+    InitialPositions(r);
     for (int i; i<N; i++)
     { 
-        ux[i]=0;
-        uy[i]=0;
-        ax[i]=0;
-        ay[i]=0;
+        u[i][0]=0;
+        u[i][1]=0;
+        a[i][0]=0;
+        a[i][1]=0;
         m[i]=1;
-        myfile <<  x[i] << "   " 
-               <<  y[i] << "   " 
-               << ux[i] << "   " 
-               << uy[i] << "\n";
+        myfile << r[i][0] << "   "
+               << r[i][1] << "   "
+               << u[i][0] << "   "
+               << u[i][1] << "\n";
     }
 
     const float T=10;
     const float dt=0.00001; 
     const int Ntimesteps=T/dt+1;
-    
+
     for (int k=0; k<N; k++)
     {
-          ComputeF(k,x,y,m,ax);
-          ComputeF(k,y,x,m,ay);
+          ComputeF(k,r,m,a);
     }
 
     for (int t=0; t<Ntimesteps; t++)
@@ -74,31 +76,27 @@ int main(int argc,char** argv)
         cout << "time=" <<  t*dt << "\n";
         for (int j=0; j<N; j++)
         {
-            ux[j]+=0.5*ax[j]*dt;
-            uy[j]+=0.5*ay[j]*dt;
-//          compute new x position        
-            x[j]+=ux[j]*dt;
-            y[j]+=uy[j]*dt;
+            u[j][0]+=0.5*a[j][0]*dt;
+            u[j][1]+=0.5*a[j][1]*dt;
+            r[j][0]+=u[j][0]*dt;
+            r[j][1]+=u[j][1]*dt;
         }
         for (int j=0; j<N; j++)
         {
-            ax[j]=0.;
-            ay[j]=0.;
+            a[j][0]=0;
+            a[j][1]=0;
         }
         for (int j=0; j<N; j++)
         {
-//          compute the new accelaration ~ force which is required for the velocity
-            ComputeF(j,x,y,m,ax);
-            ComputeF(j,y,x,m,ay);
-//          compute the new velocity
-            ux[j]+=0.5*ax[j]*dt;
-            uy[j]+=0.5*ay[j]*dt;
+            ComputeF(j,r,m,a);
+            u[j][0]+=0.5*a[j][0]*dt;
+            u[j][1]+=0.5*a[j][1]*dt;
             if(t%200==0)
             {
-                myfile <<  x[j] << "   " 
-                       <<  y[j] << "   " 
-                       << ux[j] << "   " 
-                       << uy[j] << "\n";
+                myfile << r[j][0] << "   "
+                       << r[j][1] << "   "
+                       << u[j][0] << "   "
+                       << u[j][1] << "\n";
             }
         }
     }
