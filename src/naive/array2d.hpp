b7d7bb28bbfc5711346a2e77bfc0ef1662ccfe19
Konstantinos Triantafyllou <ko.trian@gmail.com>
2018-10-17 12:08:21 +0200

    Add semi-static-semi-dynamic 2d array

--- src/naive/array2d.hpp
+++ src/naive/array2d.hpp
@@ -2,6 +2,9 @@
 #define ARRAY_2D
 
 #include <vector>
+#include <algorithm>
+
+#ifdef DYNAMIC
 
 template<class T>
 class Array2D {
@@ -34,5 +37,33 @@ class Array2D {
         size_type m_cols;
         data_type m_data;
 };
+
+#else
+
+template<class T>
+class Array2D {
+    public:
+        Array2D(size_t rows, size_t cols)
+            : m_data(new T[rows][column_size])
+        {}
+
+        Array2D(size_t rows, size_t cols, const T& val)
+            : m_data(new T[rows][column_size])
+        {
+            std::fill(&m_data[0][0], &m_data[0][0] + rows*column_size, val);
+        }
+
+        T& operator() (size_t const row, size_t const column)
+        {
+            return m_data[row][column];
+        }
+
+    private:
+        static const size_t column_size = 2;
+
+        T (*m_data)[column_size];
+};
+
+#endif // DYNAMIC
 
 #endif // ARRAY_2D 
