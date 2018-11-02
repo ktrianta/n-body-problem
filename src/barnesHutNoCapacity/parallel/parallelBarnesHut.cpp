a5a1ace12608c4c8406c3cd8fc24ca0ac1285bf7
tasos papageorgiou <tasos_pap@MacBook-Pro-tou-chreste-tasos.local>
2018-11-03 00:49:45 +0100

    parallel barnes hut

---
+++ src/barnesHutNoCapacity/parallel/parallelBarnesHut.cpp
@@ -0,0 +1,217 @@
+#include <math.h> //for sqrt function
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "../../utils/types.hpp"
+#include "../../utils/initialization.hpp"
+#include "quadtree.hpp"
+#include <unistd.h>
+#include <mpi.h>
+
+using namespace std;
+
+int N = 5;      // the number of particles
+double theta = 0.0;      // the number of particles
+const sim_data_type g = 1;     // gravitational constant
+const sim_data_type epsilon = 0.001;
+const sim_data_type epsilon2 = epsilon * epsilon;
+
+
+void writeDataToFile(sim_data_type (*r)[3], sim_data_type (*u)[3], ofstream& file)
+{
+    for (int i = 0; i < N; i++)
+    {
+        file << r[i][0] << "   "
+             << r[i][1] << "   "
+             << r[i][2] << "   "
+             << u[i][0] << "   "
+             << u[i][1] << "   "
+             << u[i][2] << "\n";
+    }
+}
+int main(int argc, char** argv)
+{
+    // ----- MPI ----- //
+    int size,rank;
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD,&size);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+//  the center of the parent node and the half width and height
+    double xc, yc, zc, h2, w2, t2;
+// coordinates for tab8096
+    xc = -2.5;
+    yc = -2.5;
+    zc = -2.5;
+    w2 = 22.5;
+    h2 = 17.5;
+    t2 = 22.5;
+
+    int c;
+    sim_data_type T = 1;
+    sim_data_type dt = 0.00001;
+    string filename;
+
+    while ((c = getopt (argc, argv, "n:t:s:i:h:")) != -1)
+    {
+        switch (c)
+        {
+            case 'n':
+                N = atoi(optarg);
+                break;
+            case 't':
+                T = atof(optarg);
+                break;
+            case 's':
+                dt = atof(optarg);
+                break;
+            case 'i':
+                filename = optarg;
+                break;
+            case 'h':
+                theta = atof(optarg);
+                break;
+        }
+    }
+
+    sim_data_type (*r)[3] = new sim_data_type[N][3];
+    sim_data_type (*u)[3] = new sim_data_type[N][3];
+    sim_data_type (*a)[3] = new sim_data_type[N][3];
+    std::fill(&u[0][0], &u[0][0] + N*3, 0);
+    std::fill(&a[0][0], &a[0][0] + N*3, 0);
+    vector<sim_data_type> m(N, 1.0/N);
+    
+
+
+    if (rank == 0)
+    {
+            if (!filename.empty()) {
+                ifstream ifile;
+            ifile.open(filename);
+
+            for (int i = 0; i < N; i++) {
+                ifile >> m[i] >> r[i][0] >> r[i][1] >> r[i][2] >> u[i][0] >> u[i][1] >> u[i][2];
+            }
+        } else {
+              initializePositionOnSphere(N, r);
+        }
+    }
+
+
+    // SEND the position vector r from Process 0 to all processes.
+    MPI_Bcast(&r[0][0],N*3, MPI_DOUBLE,0, MPI_COMM_WORLD);
+    MPI_Bcast(&u[0][0],N*3, MPI_DOUBLE,0, MPI_COMM_WORLD);
+    MPI_Bcast(&m[0],N, MPI_DOUBLE,0, MPI_COMM_WORLD);
+
+
+
+
+    ofstream file;
+    if (rank == 0)
+    {
+        file.open("output.dat");
+        writeDataToFile(r, u, file);
+    }
+    
+    QuadTree tree = QuadTree(r, m, N, xc, yc, zc, w2, h2, t2);
+
+    const int Ntimesteps = T/dt + 1;
+    // Local Declarations
+    int *local_N = new int[size];
+    int *local_Nx3 = new int[size];
+    int local_N_int = N/size;
+    int rem = N - local_N_int * size;
+    int counter = 0;
+    for (int i=0;i<size;i++)
+    {
+        local_N[i] = local_N_int;
+        if (counter < rem) 
+        {
+                local_N[i] += 1;
+                counter ++;
+        }
+        local_Nx3[i] = local_N[i]*3;
+    }
+    
+    sim_data_type (*u_local)[3] = new sim_data_type[local_N[rank]][3];
+    sim_data_type (*r_local)[3] = new sim_data_type[local_N[rank]][3];	
+    sim_data_type (*a_local)[3] = new sim_data_type[local_N[rank]][3];	
+    
+    int *offset = new int[size];
+    offset[0] = 0;
+    int *offset_x3 = new int[size];
+    offset_x3[0] = 0;
+
+    for (int i = 1; i < size; i++)
+    {
+        offset[i] = offset[i-1] + local_N[i-1];
+        offset_x3[i] = offset_x3[i-1] + local_Nx3[i-1];
+    }
+    
+    for (int i = 0; i < local_N[rank]; i++)
+    { 
+        u_local[i][0] = u[offset[rank]+i][0];
+        u_local[i][1] = u[offset[rank]+i][1];
+        u_local[i][2] = u[offset[rank]+i][2];
+        r_local[i][0] = r[offset[rank]+i][0];
+        r_local[i][1] = r[offset[rank]+i][1];
+        r_local[i][2] = r[offset[rank]+i][2];
+        a_local[i][0] = a[offset[rank]+i][0];
+        a_local[i][1] = a[offset[rank]+i][1];
+        a_local[i][2] = a[offset[rank]+i][2];
+    }
+    
+    for (int j = offset[rank]; j < local_N[rank] + offset[rank]; j++)
+    {
+        int jLocal=j-offset[rank];
+        tree.computeAcceleration(j,jLocal, r, a_local, g, theta); // NEEDS TO BE PARALLELIZED
+    }
+
+    
+            double start = 0;
+            double end = 0;
+            double time = 0;
+    for (int t = 0; t < Ntimesteps; t++)                                                         
+    {    
+        for (int j = offset[rank]; j < local_N[rank] + offset[rank]; j++)                                                              
+        {                
+            int jLocal=j-offset[rank];
+            u_local[jLocal][0] += 0.5 * a_local[jLocal][0] * dt;                                                  
+            u_local[jLocal][1] += 0.5 * a_local[jLocal][1] * dt;                                                        
+            u_local[jLocal][2] += 0.5 * a_local[jLocal][2] * dt;                                                        
+            r_local[jLocal][0] += u_local[jLocal][0] * dt;                                                              
+            r_local[jLocal][1] += u_local[jLocal][1] * dt;                                                      
+            r_local[jLocal][2] += u_local[jLocal][2] * dt;                                                      
+         }
+
+        MPI_Allgatherv(&(r_local[0][0]),local_N[rank]*3,MPI_DOUBLE,&(r[0][0]),local_Nx3, offset_x3, MPI_DOUBLE,MPI_COMM_WORLD);
+
+        for (int j = offset[rank]; j < local_N[rank] + offset[rank]; j++)                                                              
+        {
+            int jLocal=j-offset[rank];
+            start = MPI_Wtime();
+            tree.computeAcceleration(j, jLocal, r, a_local, g, theta);                                                             
+            end = MPI_Wtime();
+            time += end - start;
+
+            u_local[jLocal][0] += 0.5 * a_local[jLocal][0] * dt;                           
+            u_local[jLocal][1] += 0.5 * a_local[jLocal][1] * dt;                       
+            u_local[jLocal][2] += 0.5 * a_local[jLocal][2] * dt;                       
+            a_local[jLocal][0] = 0;
+            a_local[jLocal][1] = 0;
+            a_local[jLocal][2] = 0;
+        }                                                                                         
+        QuadTree tree = QuadTree(r, m, N, xc, yc, zc, w2, h2, t2);
+                                                                                                  
+        if (t % 600 == 0 && rank == 0)                                                                         
+        {                                                       
+            writeDataToFile(r, u, file);                       
+        }                                                                                         
+    }       
+    printf("time= %f \n", time);
+
+//  tree.print();
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ src/barnesHutNoCapacity/parallel/quadtree.cpp
@@ -0,0 +1,250 @@
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include "quadtree.hpp"
+#include <stdio.h> //for printf
+
+
+bool Rectangle::containPoint(double point[])
+{
+    if (x - w > point[0]) return false;
+    if (x + w < point[0]) return false;
+    if (y - h > point[1]) return false;
+    if (y + h < point[1]) return false;
+    if (z - t > point[2]) return false;
+    if (z + t < point[2]) return false;
+    return true;
+}
+
+
+
+// Constructor
+
+
+// Constructor for quadtree with particular size and parent -- build the tree, too!
+QuadTree::QuadTree(double (*r)[3], vector<double>& m, int N, double inp_x, double inp_y, double inp_z, double inp_hw, double inp_hh, double inp_ht)
+{
+    initialization(NULL, r, m, inp_x, inp_y, inp_z, inp_hw, inp_hh, inp_ht);
+    fill(N);
+}
+
+// Constructor for quadtree with particular size and parent -- build the tree, too!
+QuadTree::QuadTree(QuadTree* inp_parent, double (*r)[3], vector<double>& m, int N, double inp_x, double inp_y, double inp_z, double inp_hw, double inp_hh, double inp_ht)
+{
+    initialization(inp_parent, r, m, inp_x, inp_y, inp_z, inp_hw, inp_hh, inp_ht);
+    fill(N);
+}
+
+/* we dont need these two constructors
+// Constructor for quadtree with particular size (do not fill the tree)
+QuadTree::QuadTree(double (*r)[2], double inp_x, double inp_y, double inp_hw, double inp_hh)
+{
+    initialization(NULL, data, inp_x, inp_y, inp_hw, inp_hh);
+}
+*/
+// Constructor for quadtree with particular size and parent (do not fill the tree)
+QuadTree::QuadTree(QuadTree* inp_parent, double (*r)[3], vector<double>& m, double inp_x, double inp_y, double inp_z, double inp_hw, double inp_hh, double inp_ht)
+{
+    initialization(inp_parent, r, m, inp_x, inp_y, inp_z, inp_hw, inp_hh, inp_ht);
+}
+
+QuadTree::~QuadTree()
+{
+    delete fnorthWest;
+    delete fnorthEast;
+    delete fsouthWest;
+    delete fsouthEast;
+    delete bnorthWest;
+    delete bnorthEast;
+    delete bsouthWest;
+    delete bsouthEast;
+}
+
+void QuadTree::initialization(QuadTree* inp_parent, double (*r)[3], vector<double>& m, double inp_x, double inp_y, double inp_z, double inp_w, double inp_h, double inp_t){
+
+    parent = inp_parent;
+    data = r;
+    mass = m;
+    leaf = true;
+    size = 0;
+    cum_size = 0;
+    boundary.x = inp_x;
+    boundary.y = inp_y;
+    boundary.z = inp_z;
+    boundary.w = inp_w;
+    boundary.h = inp_h;
+    boundary.t = inp_t;
+    fnorthWest = NULL;
+    fnorthEast = NULL;
+    fsouthWest = NULL;
+    fsouthEast = NULL;
+    bnorthWest = NULL;
+    bnorthEast = NULL;
+    bsouthWest = NULL;
+    bsouthEast = NULL;
+    centerMass[0] = 0.;
+    centerMass[1] = 0.;
+    centerMass[2] = 0.;
+    cum_Mass = 0;
+}
+
+bool QuadTree::insert(int new_index){
+
+    double point[3];
+    point[0] = data[new_index][0];
+    point[1] = data[new_index][1];
+    point[2] = data[new_index][2];
+    if (!boundary.containPoint(point)) return false;
+
+    cum_size++;
+    double mult1 = (double) (cum_size - 1) / (double) cum_size;
+    double mult2 = 1.0 / (double) cum_size;
+    for (int d = 0; d < dimension; d++) centerMass[d] *= mult1;
+    for (int d = 0; d < dimension; d++) centerMass[d] += mult2 * point[d];
+    for (int k = 0; k < cum_size;  k++) cum_Mass += mass[k];
+    
+    if (leaf && size==0){
+        index=new_index;
+        size++;
+        return true;
+    }
+
+    if (leaf) subdivide();
+
+    if(fnorthWest->insert(new_index)) return true;
+    if(fnorthEast->insert(new_index)) return true;
+    if(fsouthWest->insert(new_index)) return true;
+    if(fsouthEast->insert(new_index)) return true;
+    if(bnorthWest->insert(new_index)) return true;
+    if(bnorthEast->insert(new_index)) return true;
+    if(bsouthWest->insert(new_index)) return true;
+    if(bsouthEast->insert(new_index)) return true;
+
+    return false;
+}
+
+void QuadTree::subdivide(){
+
+    fnorthWest = new QuadTree(this, data, mass, boundary.x - .5 * boundary.w, boundary.y - .5 * boundary.h, boundary.z + .5 * boundary.t, .5 * boundary.w, .5 * boundary.h, .5 * boundary.t);
+    fnorthEast = new QuadTree(this, data, mass, boundary.x + .5 * boundary.w, boundary.y - .5 * boundary.h, boundary.z + .5 * boundary.t, .5 * boundary.w, .5 * boundary.h, .5 * boundary.t);
+    fsouthWest = new QuadTree(this, data, mass, boundary.x - .5 * boundary.w, boundary.y + .5 * boundary.h, boundary.z + .5 * boundary.t, .5 * boundary.w, .5 * boundary.h, .5 * boundary.t);
+    fsouthEast = new QuadTree(this, data, mass, boundary.x + .5 * boundary.w, boundary.y + .5 * boundary.h, boundary.z + .5 * boundary.t, .5 * boundary.w, .5 * boundary.h, .5 * boundary.t);
+    bnorthWest = new QuadTree(this, data, mass, boundary.x - .5 * boundary.w, boundary.y - .5 * boundary.h, boundary.z - .5 * boundary.t, .5 * boundary.w, .5 * boundary.h, .5 * boundary.t);
+    bnorthEast = new QuadTree(this, data, mass, boundary.x + .5 * boundary.w, boundary.y - .5 * boundary.h, boundary.z - .5 * boundary.t, .5 * boundary.w, .5 * boundary.h, .5 * boundary.t);
+    bsouthWest = new QuadTree(this, data, mass, boundary.x - .5 * boundary.w, boundary.y + .5 * boundary.h, boundary.z - .5 * boundary.t, .5 * boundary.w, .5 * boundary.h, .5 * boundary.t);
+    bsouthEast = new QuadTree(this, data, mass, boundary.x + .5 * boundary.w, boundary.y + .5 * boundary.h, boundary.z - .5 * boundary.t, .5 * boundary.w, .5 * boundary.h, .5 * boundary.t);
+    
+    // Move existing points to correct children
+//    for(int i = 0; i < size; i++) {
+        bool success = false;
+        if (!success) success = fnorthWest->insert(index);
+        if (!success) success = fnorthEast->insert(index);
+        if (!success) success = fsouthWest->insert(index);
+        if (!success) success = fsouthEast->insert(index);
+        if (!success) success = bnorthWest->insert(index);
+        if (!success) success = bnorthEast->insert(index);
+        if (!success) success = bsouthWest->insert(index);
+        if (!success) success = bsouthEast->insert(index);
+        index = -1;
+//    }
+    
+    // Empty parent node
+    size = 0;
+    leaf = false;
+
+}
+
+void QuadTree::fill(int N)
+{
+    for (int i = 0; i < N; i++) insert(i);
+}
+
+// Print out tree
+void QuadTree::print() 
+{
+    if(cum_size == 0) {
+        printf("Empty node\n");
+        return;
+    }
+
+    if(leaf) {
+        printf("Leaf node; data = [");
+//        for(int i = 0; i < size; i++) {
+            double point[3];
+            point[0] = data[index][0];
+            point[1] = data[index][1];
+            point[2] = data[index][2];
+
+            for(int d = 0; d < dimension; d++) printf("%f, ", point[d]);
+//            if(i < size - 1) printf("\n");
+            printf("]\n");
+//        }        
+    }
+    else {
+        printf("Intersection node with center-of-mass = [");
+        for(int d = 0; d < dimension; d++) printf("%f, ", centerMass[d]);
+        printf("]; children are:\n");
+        fnorthEast->print();
+        fnorthWest->print();
+        fsouthEast->print();
+        fsouthWest->print();
+        bnorthEast->print();
+        bnorthWest->print();
+        bsouthEast->print();
+        bsouthWest->print();
+    }
+}
+
+
+void QuadTree::computeAcceleration(int idxGlobal, int idxLocal, double (*r)[3], double (*a)[3], double g, double theta) {
+    if (cum_size == 0 || (leaf == true && size == 1 && index == idxGlobal)) {
+        return;
+    }
+
+    
+    double d = sqrt( (centerMass[0] - r[idxGlobal][0]) * (centerMass[0] - r[idxGlobal][0])
+                   + (centerMass[1] - r[idxGlobal][1]) * (centerMass[1] - r[idxGlobal][1])
+                   + (centerMass[2] - r[idxGlobal][2]) * (centerMass[2] - r[idxGlobal][2]));
+
+    if (2.*boundary.w/d <= theta) {
+        
+        double rji[3];
+        rji[0] = centerMass[0] - r[idxGlobal][0];
+        rji[1] = centerMass[1] - r[idxGlobal][1];
+        rji[2] = centerMass[2] - r[idxGlobal][2];
+        double r2 = rji[0] * rji[0] + rji[1] * rji[1] + rji[2] * rji[2];
+        double denom = r2 * sqrt(r2);
+        double a_i = -g * cum_Mass / denom;
+        a[idxLocal][0] -= a_i * rji[0];
+        a[idxLocal][1] -= a_i * rji[1];
+        a[idxLocal][2] -= a_i * rji[2];
+        return;
+    }
+    if (leaf == true) {
+//        for(int i = 0; i < size; i++) {
+            int idx_i = index;
+            if (idxGlobal == idx_i) return;
+
+            double rji[3];
+            rji[0] = r[idx_i][0] - r[idxGlobal][0];
+            rji[1] = r[idx_i][1] - r[idxGlobal][1];
+            rji[2] = r[idx_i][2] - r[idxGlobal][2];
+            double r2 = rji[0] * rji[0] + rji[1] * rji[1] + rji[2] * rji[2];
+            double denom = r2 * sqrt(r2);
+            double a_i = -g * mass[idx_i] / denom;
+            a[idxLocal][0] -= a_i * rji[0];
+            a[idxLocal][1] -= a_i * rji[1];
+            a[idxLocal][2] -= a_i * rji[2];
+//        }
+    } else {
+        fnorthWest->computeAcceleration(idxGlobal, idxLocal, r, a, g, theta);
+        fnorthEast->computeAcceleration(idxGlobal, idxLocal, r, a, g, theta);
+        fsouthWest->computeAcceleration(idxGlobal, idxLocal, r, a, g, theta);
+        fsouthEast->computeAcceleration(idxGlobal, idxLocal, r, a, g, theta);
+        fnorthWest->computeAcceleration(idxGlobal, idxLocal, r, a, g, theta);
+        bnorthEast->computeAcceleration(idxGlobal, idxLocal, r, a, g, theta);
+        bsouthWest->computeAcceleration(idxGlobal, idxLocal, r, a, g, theta);
+        bsouthEast->computeAcceleration(idxGlobal, idxLocal, r, a, g, theta);
+    }
+}
+
