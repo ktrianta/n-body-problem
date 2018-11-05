b57965e52c0b6eae2fafbb9ee75886f2bd4e557d
Konstantinos Triantafyllou <ko.trian@gmail.com>
2018-11-05 23:30:45 +0100

    Fix parallel one-sided code

--- src/naive/parallel_one_sided/main.cpp
+++ src/naive/parallel_one_sided/main.cpp
@@ -5,57 +5,65 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include "../utils/types.hpp"
-#include "../utils/initialization.hpp"
+#include "types.hpp"
+#include "initialization.hpp"
 #include <unistd.h>
 #include <mpi.h>
 
 using namespace std;
 
 int N = 6;      // the number of particles
-const sim_data_type g = 1;     // gravitational constant
-const sim_data_type epsilon = 0.001;
-const sim_data_type epsilon2 = epsilon * epsilon;
+const sim::data_type g = 1;     // gravitational constant
+const sim::data_type epsilon = 0.001;
+const sim::data_type epsilon2 = epsilon * epsilon;
 
-void computeAcceleration(sim_data_type (*r)[2], sim_data_type (*a)[2], vector<sim_data_type>& m, int rank, int local_N, int offset)
+void computeAcceleration(sim::data_type (*r)[3], sim::data_type (*a)[3], vector<sim::data_type>& m, int rank, int local_N, int offset)
 {
     for (int i = 0; i < N; i++)
     {
         a[i][0] = 0;
         a[i][1] = 0;
+        a[i][2] = 0;
     }
 
     for (int i = offset; i < offset + local_N; i++)
     {
-        sim_data_type a_i0 = 0;  // accumulate accelaration values for particle i and
-        sim_data_type a_i1 = 0;  // store them at the end of the loop iteration in a(i,x)
+        sim::data_type a_i0 = 0;  // accumulate accelaration values for particle i and
+        sim::data_type a_i1 = 0;  // store them at the end of the loop iteration in a(i,x)
+        sim::data_type a_i2 = 0;
+
         for (int j = 0; j < N; j++)
         {
             if (i==j) continue;
 	
-            sim_data_type rji[2];
+            sim::data_type rji[3];
             rji[0] = r[j][0] - r[i][0];
             rji[1] = r[j][1] - r[i][1];
-            sim_data_type r2 = rji[0] * rji[0] + rji[1] * rji[1];
-            sim_data_type denom = (r2+epsilon2) * sqrt(r2+epsilon2);
-            sim_data_type a_i = -g * m[j] / denom;
+            rji[2] = r[j][2] - r[i][2];
+            sim::data_type r2 = rji[0] * rji[0] + rji[1] * rji[1] + rji[2] * rji[2];
+            sim::data_type denom = (r2+epsilon2) * sqrt(r2+epsilon2);
+            sim::data_type a_i = -g * m[j] / denom;
             a_i0 -= a_i * rji[0];
             a_i1 -= a_i * rji[1];
+            a_i2 -= a_i * rji[2];
         
         }
         a[i][0] += a_i0;  // a(i, 0) and a(i, 1) are accessed once here, avoiding
         a[i][1] += a_i1;  // repeated accesses in the inner loop of j
+        a[i][2] += a_i2;
     }
 }
 
-void writeDataToFile(sim_data_type (*r)[2], sim_data_type (*u)[2], ofstream& file)
+void writeDataToFile(sim::data_type (*r)[3], sim::data_type (*u)[3], ofstream& file)
 {
     for (int i = 0; i < N; i++)
     {
         file << r[i][0] << "   "
              << r[i][1] << "   "
+             << r[i][2] << "   "
              << u[i][0] << "   "
-             << u[i][1] << "\n";
+             << u[i][1] << "   "
+             << u[i][2] << "\n";
     }
 }
 
@@ -73,8 +81,8 @@ int main(int argc, char** argv)
 
 
     int c;
-    sim_data_type T = 10;
-    sim_data_type dt = 0.00001;
+    sim::data_type T = 10;
+    sim::data_type dt = 0.00001;
     string filename;
 
     while ((c = getopt (argc, argv, "n:t:s:i:")) != -1)
@@ -96,14 +104,14 @@ int main(int argc, char** argv)
         }
     }
 
-    sim_data_type (*r)[2] = new sim_data_type[N][2];
-    sim_data_type (*r1)[2] = new sim_data_type[N][2];
-    sim_data_type (*u)[2] = new sim_data_type[N][2];
-    sim_data_type (*a)[2] = new sim_data_type[N][2];
-    std::fill(&u[0][0], &u[0][0] + N*2, 0);
-    std::fill(&r1[0][0], &r1[0][0] + N*2, 0);
-    std::fill(&a[0][0], &a[0][0] + N*2, 0);
-    vector<sim_data_type> m(N, 1.0/N);
+    sim::data_type (*r)[3] = new sim::data_type[N][3];
+    sim::data_type (*r1)[3] = new sim::data_type[N][3];
+    sim::data_type (*u)[3] = new sim::data_type[N][3];
+    sim::data_type (*a)[3] = new sim::data_type[N][3];
+    std::fill(&u[0][0], &u[0][0] + N*3, 0);
+    std::fill(&r1[0][0], &r1[0][0] + N*3, 0);
+    std::fill(&a[0][0], &a[0][0] + N*3, 0);
+    vector<sim::data_type> m(N, 1.0/N);
     
     if (rank==0)
     {
@@ -112,17 +120,17 @@ int main(int argc, char** argv)
         ifile.open(filename);
 
             for (int i = 0; i < N; i++) {
-                ifile >> m[i] >> r[i][0] >> r[i][1] >> u[i][0] >> u[i][1];
+                ifile >> m[i] >> r[i][0] >> r[i][1] >> r[i][2] >> u[i][0] >> u[i][1] >> u[i][2];
             }
         } else {
             initializePositionOnSphere(N, r);
         }
     }
-    MPI_Bcast (&r[0][0], 2*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);    
-    MPI_Bcast (&u[0][0], 2*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);    
+    MPI_Bcast (&r[0][0], 3*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast (&u[0][0], 3*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast (&m[0], N, MPI_DOUBLE, 0, MPI_COMM_WORLD);    
 
-    MPI_Win_create( &(r[0][0]), sizeof(double)*2*N, sizeof(double), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+    MPI_Win_create( &(r[0][0]), sizeof(double)*3*N, sizeof(double), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
     ofstream file;
     file.open("output.dat");
 
@@ -140,7 +148,7 @@ int main(int argc, char** argv)
                 local_N[i] += 1;
                 counter ++;
         }
-        local_Nx2[i] = local_N[i]*2;
+        local_Nx2[i] = local_N[i]*3;
     }
     int offset[size];
     offset[0] = 0;
@@ -164,8 +172,10 @@ int main(int argc, char** argv)
         {
             u[j][0] += 0.5 * a[j][0] * dt;
             u[j][1] += 0.5 * a[j][1] * dt;
+            u[j][2] += 0.5 * a[j][2] * dt;
             r[j][0] += u[j][0] * dt;
             r[j][1] += u[j][1] * dt;
+            r[j][2] += u[j][2] * dt;
             //printf("%lf %lf %d \n", r[j][0], r[j][1], rank);
 		
         }
@@ -188,6 +198,7 @@ int main(int argc, char** argv)
         {
             u[j][0] += 0.5 * a[j][0] * dt;
             u[j][1] += 0.5 * a[j][1] * dt;
+            u[j][2] += 0.5 * a[j][2] * dt;
         }
 	//Needed for correct u in output.dat
         //MPI_Allgather(MPI_IN_PLACE, 0, MPI_DATATYPE_NULL,&(u[0][0]),local_N*2,MPI_DOUBLE,MPI_COMM_WORLD);
