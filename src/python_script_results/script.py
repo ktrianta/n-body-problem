b219062ebf6af5759a618ccd12b696e8cdccdfef
RamyTanios <ramytanios@RamyTanioss-MacBook-Pro.local>
2018-11-09 22:53:50 +0100

    updates

--- src/python_script_results/script.py
+++ src/python_script_results/script.py
@@ -14,16 +14,23 @@ theta  = float(sys.argv[4])
 if num_of_columns == 3: 
     rx_me,ry_me,rz_me = genfromtxt(filename1,unpack=True,delimiter=' ',skip_header=0,skip_footer=0)
     rx_other,ry_other,rz_other = genfromtxt(filename2,unpack=True,delimiter=' ',skip_header=0,skip_footer=0)
-else if num_of_colums == 6:
+elif num_of_colums == 6:
     rx_me,ry_me,rz_me,vx_me,vy_me,vz_me = genfromtxt(filename1,unpack=True,delimiter=' ',skip_header=0,skip_footer=0)
     rx_other,ry_other,rz_other,vx_other,vy_other,vz_other = genfromtxt(filename2,unpack=True,delimiter=' ',skip_header=0,skip_footer=0)
 
-error = abs(X-Y)
+# For accuracy, use np.square(), for speed use **2
+r_me = sqrt(rx_me**2+ry_me**2+rz_me**2)
+r_other = sqrt(rx_other**2+ry_other**2+rz_other**2)
 
-L2_error = LA.norm(error,2)     # sqrt(sum(e_i),1<=i<=num_particules)
-LInf_error = LA.norm(error,Inf) # max [e_i], 1<=i<=num_particules
+#v_me = sqrt(vx_me**2+vy_me**2+vz_me**2)
+#v_other = sqrt(vx_other**2+vy_other**2+vz_other**2)
 
-if ( L2_error > theta ) :
+difference_error_r = abs(r_me - r_other)
+
+L2_error_r = LA.norm(difference_error_r, 2)     # sqrt(sum(e_i),1<=i<=num_particules)
+LInf_error_r = LA.norm(difference_error_r, Inf) # max [e_i], 1<=i<=num_particules
+
+if ( L2_error_r > theta ) :
     print("No convergence")
 else:
     print("The solution is accurate")
