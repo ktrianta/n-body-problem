b512f44ae9bbe60191cb19cc6ed26796d7fbad14
RamyTanios <ramytanios@public-docking-hg-8005.ethz.ch>
2018-11-16 16:00:59 +0100

    Working version of dynamic windows and  struct treeNode

--- src/testing_tree_dynamic_window/main.cpp
+++ src/testing_tree_dynamic_window/main.cpp
@@ -52,53 +52,68 @@ int main(int argc, char** argv){
 	MPI_Win win;
 	MPI_Win_create_dynamic(MPI_INFO_NULL, MPI_COMM_WORLD, &win);
 
-	int NT = 1;
-	int NNPT=1;
+	int NT = 2;
+	int NNPT= 2 ;
 
 	Treenode **t = new Treenode*[NNPT];
 	for (int i=0; i<NNPT; i++)
 		t[i] = new Treenode[NT];
 
-	MPI_Aint* my_disp = new MPI_Aint[NT];
-
-	MPI_Aint **target_disp = new MPI_Aint*[NT];
-	for (int i=0; i<NT ; i++){
-		target_disp[i] = new MPI_Aint[comm_size];
-		MPI_Get_address(&t[i],&my_disp[i]);
-	}	
-
-	MPI_Allgather(my_disp, NT, MPI_AINT, &target_disp[0][0], NT, MPI_AINT, MPI_COMM_WORLD);
-	
 	for (int i=0; i<NT; i++){
-		MPI_Alloc_mem(NNPT*sizeof(struct Treenode), MPI_INFO_NULL, &t[i]);
+		MPI_Alloc_mem(NNPT*sizeof(struct Treenode), MPI_INFO_NULL, &t[i][0]);
 		MPI_Win_attach(win,t[i],sizeof(struct Treenode)); 
 	}
 	
+	MPI_Aint* my_disp = new MPI_Aint[NT];
 
-//	MPI_Aint my_displ[2];
-//	MPI_Aint (*target_displ)[2] = new MPI_Aint[comm_size][2];
-//	MPI_Get_address(t,&my_displ[0]);
-//	MPI_Get_address(t1,&my_displ[1]);
+	MPI_Aint **target_disp = new MPI_Aint*[NT];
+	for (int i=0; i<NT; i++){
+		target_disp[i] = new MPI_Aint[comm_size];
+		MPI_Get_address(&t[i][0],&my_disp[i]);
+	}
 
+	MPI_Allgather(my_disp, NT, MPI_AINT, &target_disp[0][0], NT, MPI_AINT, MPI_COMM_WORLD);
+	
 	if (rank == 0){
-		t[0][0].x = 1.23; t[0][0].y = 4.1; t[0][0].z = 1.41; t[0][0].w = 25.1;
-		t[0][0].h = 1.21; t[0][0].t = 7.2;
-		t[0][0].mass = 27.2; t[0][0].massCenter[0] = 1.1;t[0][0].massCenter[1] = 1.2;
-		t[0][0].massCenter[2] = 1.3;
-		t[0][0].index = 10; t[0][0].leaf = true; t[0][0].cum_size = 31; t[0][0].child = 41;
+		for (int i = 0 ; i < NNPT ; i ++){
+			for (int j = 0 ; j < NT; j++){
+				t[i][j].x = 1.23; t[i][j].y = 4.1; t[i][j].z = 1.41; t[i][j].w = 25.1;
+				t[i][j].h = 1.21; t[i][j].t = 7.2;
+				t[i][j].mass = 27.2; t[i][j].massCenter[0] = 1.1;t[i][j].massCenter[1] = 1.2;
+				t[i][j].massCenter[2] = 1.3;
+				t[i][j].index = 10; t[i][j].leaf = true; t[i][j].cum_size = 31; t[i][j].child = 41;
+			}
+		}
 	}
 
 	MPI_Win_fence(0,win);
 	if (rank == 1){
-		MPI_Get(&t[0][0],1,treeNodeStruct,0,target_disp[0][0],1,treeNodeStruct,win);
+		MPI_Get(&t[0][0],3,treeNodeStruct,0,target_disp[0][0],3,treeNodeStruct,win);
 	}	
 	MPI_Win_fence(0,win);
 	
 	if (rank == 1){
 		std::cout<<t[0][0].x<<" "<<t[0][0].y<<" "<<t[0][0].z<<" "<<t[0][0].w<<" "<<std::endl;
 		std::cout<<t[0][0].h<<" "<<t[0][0].t<<" "<<t[0][0].mass<<" "<<t[0][0].massCenter[0]<<" "<<std::endl;
-		std::cout<<t[0][0].massCenter[1]<<" "<<t[0][0].massCenter[2]<<" "<<t[0][0].index<<" "<<t[0][0].leaf<<" "<<std::endl;
+		std::cout<<t[0][0].massCenter[0]<<" "<<t[0][0].massCenter[2]<<" "<<t[0][0].index<<" "<<t[0][0].leaf<<" "<<std::endl;
 		std::cout<<t[0][0].cum_size<<" "<<t[0][0].child<<std::endl;
+	
+		std::cout<<t[0][1].x<<" "<<t[0][1].y<<" "<<t[0][1].z<<" "<<t[0][1].w<<" "<<std::endl;
+		std::cout<<t[0][1].h<<" "<<t[0][1].t<<" "<<t[0][1].mass<<" "<<t[0][1].massCenter[0]<<" "<<std::endl;
+		std::cout<<t[0][1].massCenter[0]<<" "<<t[0][1].massCenter[2]<<" "<<t[0][1].index<<" "<<t[0][1].leaf<<" "<<std::endl;
+		std::cout<<t[0][1].cum_size<<" "<<t[0][1].child<<std::endl;
+	
+		std::cout<<t[1][0].x<<" "<<t[1][0].y<<" "<<t[1][0].z<<" "<<t[1][0].w<<" "<<std::endl;
+		std::cout<<t[1][0].h<<" "<<t[1][0].t<<" "<<t[1][0].mass<<" "<<t[1][0].massCenter[0]<<" "<<std::endl;
+		std::cout<<t[1][0].massCenter[0]<<" "<<t[1][0].massCenter[2]<<" "<<t[1][0].index<<" "<<t[1][0].leaf<<" "<<std::endl;
+		std::cout<<t[1][0].cum_size<<" "<<t[1][0].child<<std::endl;
+	
+		std::cout<<t[1][1].x<<" "<<t[1][1].y<<" "<<t[1][1].z<<" "<<t[1][1].w<<" "<<std::endl;
+		std::cout<<t[1][1].h<<" "<<t[1][1].t<<" "<<t[1][1].mass<<" "<<t[1][1].massCenter[0]<<" "<<std::endl;
+		std::cout<<t[1][1].massCenter[0]<<" "<<t[1][1].massCenter[2]<<" "<<t[1][1].index<<" "<<t[1][1].leaf<<" "<<std::endl;
+		std::cout<<t[1][1].cum_size<<" "<<t[1][1].child<<std::endl;
+	
+	
 	}
 	
 	MPI_Win_detach(win,t);free(t);
