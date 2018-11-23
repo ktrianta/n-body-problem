9f5a98ad5401158231c4db8fe9b7ca734ac9ebaa
Konstantinos Triantafyllou <ko.trian@gmail.com>
2018-11-23 14:31:57 +0100

    Add new readDataFromFile function

--- src/utils/io.cpp
+++ src/utils/io.cpp
@@ -1,8 +1,7 @@
 #include "io.hpp"
 
 
-std::string baseName(std::string const& path)
-{
+std::string baseName(std::string const& path) {
     return path.substr(path.find_last_of("/") + 1);
 }
 
@@ -10,6 +9,7 @@ void openFileToWrite(std::ofstream& fs, std::string filename, std::string dirnam
     std::string out_filename = dirname + "/" + baseName(filename) + ".out";
     fs.open(out_filename);
 }
+
 
 
 int readDataFromFile(std::string filename, int N, dtype *m, dtype (*r)[3], dtype (*u)[3]) {
@@ -30,9 +30,25 @@ int readDataFromFile(std::string filename, int N, dtype *m, dtype (*r)[3], dtype
     return 0;
 }
 
+int readDataFromFile(std::string filename, int N, dtype (*a)[7]) {
+    std::ifstream fs;
 
-void writeDataToFile(int N, dtype (*a)[3], std::ofstream& fs)
-{
+    fs.open(filename);
+    if (!fs.is_open()) {
+        return -1;
+    }
+
+    for (size_t i = 0; i < N; i++) {
+        fs >> a[i][0]
+           >> a[i][1] >> a[i][2] >> a[i][3]
+           >> a[i][4] >> a[i][5] >> a[i][6];
+    }
+
+    fs.close();
+    return 0;
+}
+
+void writeDataToFile(int N, dtype (*a)[3], std::ofstream& fs) {
     for (size_t i = 0; i < N; i++) {
         fs << a[i][0] << "    "
            << a[i][1] << "    "
@@ -41,8 +57,7 @@ void writeDataToFile(int N, dtype (*a)[3], std::ofstream& fs)
 }
 
 
-void writeDataToFile(int N, dtype (*a)[3], dtype (*b)[3], std::ofstream& fs)
-{
+void writeDataToFile(int N, dtype (*a)[3], dtype (*b)[3], std::ofstream& fs) {
     for (size_t i = 0; i < N; i++) {
         fs << a[i][0] << "    "
            << a[i][1] << "    "
--- src/utils/io.hpp
+++ src/utils/io.hpp
@@ -9,6 +9,7 @@ typedef sim::data_type dtype;
 
 void openFileToWrite(std::ofstream& file, std::string filename, std::string dirname = ".");
 int readDataFromFile(std::string fname, int N, dtype *m, dtype (*r)[3], dtype (*u)[3]);
+int readDataFromFile(std::string fname, int N, dtype (*a)[7]);
 void writeDataToFile(int N, dtype (*a)[3], std::ofstream& fs);
 void writeDataToFile(int N, dtype (*a)[3], dtype (*b)[3], std::ofstream& fs);
 
