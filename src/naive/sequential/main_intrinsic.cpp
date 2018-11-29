b495bc7f7189719ecc18fd9477844b829e256cdd
RamyTanios <ramytanios@student-net-hg-2434.ethz.ch>
2018-11-29 10:01:29 +0100

    Naive sequential version with intrinsics

--- src/naive/sequential/main_intrinsic.cpp
+++ src/naive/sequential/main_intrinsic.cpp
@@ -5,7 +5,7 @@
 #include "args.hpp"
 #include "types.hpp"
 #include "initialization.hpp"
-#include <xmmintrin.h>
+#include <immintrin.h>
 
 void * operator new(size_t size) throw(std::bad_alloc){
    void *p;
@@ -29,25 +29,73 @@ void computeAcceleration(const size_t N, sim::data_type (*r)[3], sim::data_type 
         sim::data_type a_i1 = 0;  // store them at the end of the loop iteration in a(i,x)
         sim::data_type a_i2 = 0;
 
-        for (size_t j = i+1; j < N; j++) {
-            sim::data_type rji[3];
-            rji[0] = r[j][0] - r[i][0];
-            rji[1] = r[j][1] - r[i][1];
-            rji[2] = r[j][2] - r[i][2];
-            sim::data_type r2 = rji[0] * rji[0] + rji[1] * rji[1] + rji[2] * rji[2];
-            sim::data_type denom = (r2+sim::e2) * sqrt(r2+sim::e2);
-            sim::data_type a_j = - sim::g * m[i] / denom;
-            sim::data_type a_i = - sim::g * m[j] / denom;
-            a[j][0] += a_j * rji[0];
-            a[j][1] += a_j * rji[1];
-            a[j][2] += a_j * rji[2];
-            a_i0 -= a_i * rji[0];
-            a_i1 -= a_i * rji[1];
-            a_i2 -= a_i * rji[2];
+	__m256 ai0 = _mm256_setzero_pd();
+	__m256 ai1 = _mm256_setzero_pd();
+	__m256 ai2 = _mm256_setzero_pd();
+
+	 sim::data_type a_data[12];
+
+   sim::data_type r_data_j[12];
+   sim::data_type r_data_i[12];
+
+        for (size_t j = 0; j < N; j+=4) { /* Get contribution of all j particules on particule i. */
+
+		for (int k=0; k<4; k++){
+			/* Particule i loaded */
+			r_data_i[k]   = r[i][0];
+			r_data_i[k+4] = r[i][1];
+			r_data_i[k+8] = r[i][2];
+
+			/* Load other particules j */
+			r_data_j[k]   = r[j+k][0];
+			r_data_j[k+4] = r[j+k][1];
+			r_data_j[k+8] = r[j+k][2];
+		}
+
+		__m256 rj0   = _mm256_load_pd( r_data_j	    );
+		__m256 rj1   = _mm256_load_pd( &r_data_j[4] );
+		__m256 rj2   = _mm256_load_pd( &r_data_j[8] );
+
+		__m256 ri0   = _mm256_load_pd( r_data_i     );
+		__m256 ri1   = _mm256_load_pd( &r_data_i[4] );
+		__m256 ri2   = _mm256_load_pd( &r_data_i[8] );
+
+		__m256 sub0  = _mm256_sub_pd( rj0, ri0 );
+		__m256 sub1  = _mm256_sub_pd( rj1, ri1 );
+		__m256 sub2  = _mm256_sub_pd( rj2, ri2 );
+
+		__m256 mult0 = _mm256_mul_pd( sub0, sub0 );	// delta_x squared
+		__m256 mult1 = _mm256_mul_pd( sub1, sub1 );	// delta_y squared
+		__m256 mult2 = _mm256_mul_pd( sub2, sub2 );     // delta_z squared
+
+		__m256 add      = _mm256_add_pd( _mm256_add_pd(mult0, mult1), mult2  ); 	// delta_x squared + delta_y squared + delta_z squared
+
+		__m256 denom    = _mm256_mul_pd(_mm256_sqrt_pd(add), add);         // sqrt(expr) * expr , expr = above.
+		__m256 zeros = _mm256_set1_pd(0);
+		__m256 cmp_res = _mm256_cmpeq_epi64(zeros,denom);
+	
+		__m256 masses = _mm256_load_pd(m+j);
+		__m256 grav = _mm256_set1_pd(-sim::g);
+
+		__m256 a_i = _mm256_div_pd(_mm256_mul_pd(grav,masses),denom);
+		a_i = _mm256_andnot_si256(cmp_res, a_i);
+
+		ai0 = _mm256_sub_pd(ai0,_mm256_mul_pd(a_i,sub0));
+		ai1 = _mm256_sub_pd(ai1,_mm256_mul_pd(a_i,sub1));
+		ai2 = _mm256_sub_pd(ai2,_mm256_mul_pd(a_i,sub2));
+
+		_mm256_store_pd(a_data,ai0);
+		_mm256_store_pd(&a_data[4],ai1);
+		_mm256_store_pd(&a_data[8],ai2);
+
         }
-        a[i][0] += a_i0;  // a(i, 0) and a(i, 1) are accessed once here, avoiding
-        a[i][1] += a_i1;  // repeated accesses in the inner loop of j
-        a[i][2] += a_i2;  // repeated accesses in the inner loop of j
+		for (int k=0; k<4; k++){
+			a[i][0] += a_data[k];
+			a[i][1] += a_data[k+4];
+			a[i][2] += a_data[k+8];
+		}
+
+
     }
 }
 
