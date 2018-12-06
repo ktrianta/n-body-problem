acab13f55df25ab7fbe6443a934f76da96be86b4
tasos papageorgiou <tasos_pap@student-net-vpn-0956.ethz.ch>
2018-12-06 22:21:59 +0100

    measurements for time

--- src/naive/sequential/main.cpp
+++ src/naive/sequential/main.cpp
@@ -5,6 +5,10 @@
 #include "args.hpp"
 #include "types.hpp"
 #include "initialization.hpp"
+#include <chrono>
+
+
+using time_point_t = std::chrono::high_resolution_clock::time_point;
 
 
 void computeAcceleration(const size_t N, sim::data_type (*r)[3], sim::data_type (*a)[3], sim::data_type *m) {
@@ -38,9 +42,21 @@ void computeAcceleration(const size_t N, sim::data_type (*r)[3], sim::data_type 
 }
 
 int main(int argc, char** argv) {
+
+    time_point_t prog_start;
+    time_point_t prog_end;
+    time_point_t io_start;
+    time_point_t io_end;
+    time_point_t comp_start;
+    time_point_t comp_end;
+    double  prog_time = 0;
+    double io_time = 0;
+    double comp_time = 0;
+
+    prog_start = std::chrono::high_resolution_clock::now();
+
     sim::Parameters params;
     readArgs(argc, argv, params);
-
     const size_t N = params.n;
     sim::data_type *m = new sim::data_type[N];
     sim::data_type (*r)[3] = new sim::data_type[N][3];
@@ -50,7 +66,7 @@ int main(int argc, char** argv) {
     std::fill(&u[0][0], &u[0][0] + N*3, 0);
     std::fill(&a[0][0], &a[0][0] + N*3, 0);
 
-
+    io_start = std::chrono::high_resolution_clock::now();
     if (!params.in_filename.empty()) {
         if (readDataFromFile(params.in_filename, N, m, r, u) == -1) {
             std::cerr << "File " << params.in_filename << " not found!" << std::endl;
@@ -64,11 +80,14 @@ int main(int argc, char** argv) {
     std::ofstream out_file;
     openFileToWrite(out_file, params.out_filename, params.out_dirname);
     writeDataToFile(params.n, r, u, out_file);
+    io_end = std::chrono::high_resolution_clock::now();
+    io_time += std::chrono::duration< double >(io_end - io_start).count();
 
     computeAcceleration(params.n, r, a, m);
 
     const size_t Ntimesteps = params.t / params.dt + 1;
     const sim::data_type dt = params.dt;
+     
 
     for (size_t t = 0; t < Ntimesteps; t++) {
         for (size_t j = 0; j < N; j++) {
@@ -80,7 +99,10 @@ int main(int argc, char** argv) {
             r[j][2] += u[j][2] * dt;
         }
 
+      	comp_start = std::chrono::high_resolution_clock::now();
         computeAcceleration(N, r, a, m);
+        comp_end = std::chrono::high_resolution_clock::now();
+    	comp_time += std::chrono::duration< double >(comp_end - comp_start).count();
 
         for (size_t j = 0; j < N; j++) {
             u[j][0] += 0.5 * a[j][0] * dt;
@@ -92,5 +114,13 @@ int main(int argc, char** argv) {
             writeDataToFile(N, r, u, out_file);
         }
     }
+
+    prog_end = std::chrono::high_resolution_clock::now();
+    prog_time += std::chrono::duration< double >(prog_end - prog_start).count();
+    FILE *plotFile;
+    plotFile = fopen("plotData.txt", "a");
+    fprintf(plotFile, "%lf,  %lf, %lf \n",prog_time,  comp_time, io_time);
+    fclose(plotFile);
+
     return 0;
 }
