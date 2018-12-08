5d7f1c40c210a5b9be4ab6da4e11e1e21fdfe2f1
Konstantinos Triantafyllou <ko.trian@gmail.com>
2018-12-08 16:29:54 +0100

    Update compare script

--- scripts/compare.py
+++ scripts/compare.py
@@ -1,39 +1,46 @@
 from numpy import *
+from numpy import square
 from numpy import linalg as LA
 import sys
 
-# Sample execution command: python compare.py filename_me filename_other num_of_columns epsilon
-# Note that the file inputs should be in the same directory as the compare.py file. Otherwise, provide the path 
-# of each file in the terminal. 
+# Sample execution command: python compare.py filename_me filename_other epsilon
+# Note that the file inputs should be in the same directory as the compare.py file.
+# Otherwise, provide the path of each file in the terminal. 
 
 filename_me = sys.argv[1]
 filename_other = sys.argv[2]
-num_of_columns = int(sys.argv[3])
-epsilon  = float(sys.argv[4])
+epsilon = float(sys.argv[3])
 
-rx_me,ry_me,rz_me,vx_me,vy_me,vz_me = genfromtxt(filename_me,unpack=True,delimiter='    ')
-rx_other,ry_other,rz_other,vx_other,vy_other,vz_other = genfromtxt(filename_other,unpack=True,delimiter='    ')
+rx_me, ry_me, rz_me = genfromtxt(filename_me,unpack=True,delimiter='    ',usecols=(0,1,2))
+rx_other, ry_other, rz_other = genfromtxt(filename_other,unpack=True,delimiter='    ',usecols=(0,1,2))
 
-#For accuracy, use np.square(), for speed use **2
-r_me = sqrt(rx_me**2+ry_me**2+rz_me**2)
-r_other = sqrt(rx_other**2+ry_other**2+rz_other**2)
-v_me = sqrt(vx_me**2+vy_me**2+vz_me**2)
-v_other = sqrt(vx_other**2+vy_other**2+vz_other**2)
+# For accuracy, use np.square(), for speed use **2
+r_me = sqrt(square(rx_me) + square(ry_me) + square(rz_me))
+r_other = sqrt(square(rx_other) + square(ry_other) + square(rz_other))
 
-difference_error_r = zeros(len(r_me))
+abs_difference_error_r = zeros(len(r_me))
+rel_difference_error_r = zeros(len(r_me))
+
 for i in range(0,len(r_me)):
     diffr = r_me[i] + r_other[i]
     if ( diffr == 0 ):
-        difference_error_r[i] = 0
+        abs_difference_error_r[i] = 0
+        rel_difference_error_r[i] = 0
     else: 
-        difference_error_r[i] =  100 * 2 * abs(r_me[i] - r_other[i]) / diffr 
+        abs_difference_error_r[i] = abs(r_me[i] - r_other[i]) 
+        rel_difference_error_r[i] = 2 * abs(r_me[i] - r_other[i]) / diffr
 
+L2_error_a = LA.norm(abs_difference_error_r, 2)
+LInf_error_a = LA.norm(abs_difference_error_r, Inf)
 
-L2_error_r = LA.norm(difference_error_r, 2)     # sqrt(sum(e_i),1<=i<=num_particules)
-LInf_error_r = LA.norm(difference_error_r, Inf) # max [e_i], 1<=i<=num_particules
+print "Maximum absolute error difference:", LInf_error_a
+print "Total absolute error difference:", L2_error_a
 
-print("The error difference is: ")
-print(LInf_error_r)
+L2_error_r = LA.norm(rel_difference_error_r, 2)
+LInf_error_r = LA.norm(rel_difference_error_r, Inf)
+
+print "Maximum relative error difference:", LInf_error_r
+print "Total relative error difference:", L2_error_r
 
 if ( L2_error_r > epsilon ) :
     print("No convergence")
