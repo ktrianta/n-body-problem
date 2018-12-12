55a707be131e21c1e1c30132e68a6da9df64f456
Konstantinos Triantafyllou <ko.trian@gmail.com>
2018-12-12 19:22:48 +0000

    Change division to reciprocal in avx512

--- src/naive/avx512/main.cpp
+++ src/naive/avx512/main.cpp
@@ -36,6 +36,9 @@ void computeAcceleration(const size_t N, sim::data_type (*rx), sim::data_type (*
     sim::data_type* a_iy = new sim::data_type[8];
     sim::data_type* a_iz = new sim::data_type[8];
 
+    __m512d g = _mm512_set1_pd(-sim::g);
+    __m512d zeros = _mm512_set1_pd(0);
+
     for (size_t i = 0; i < N; i++) {
         __m512d aix = _mm512_setzero_pd();
         __m512d aiy = _mm512_setzero_pd();
@@ -49,6 +52,8 @@ void computeAcceleration(const size_t N, sim::data_type (*rx), sim::data_type (*
             __m512d rjx   = _mm512_load_pd( rx+j );
             __m512d rjy   = _mm512_load_pd( ry+j );
             __m512d rjz   = _mm512_load_pd( rz+j );
+            __m512d masses = _mm512_load_pd(m+j);
+            __m512d mg = _mm512_mul_pd(g,masses);
 
             __m512d subx  = _mm512_sub_pd( rjx, rix );
             __m512d suby  = _mm512_sub_pd( rjy, riy );
@@ -60,19 +65,13 @@ void computeAcceleration(const size_t N, sim::data_type (*rx), sim::data_type (*
 
             __m512d add   = _mm512_add_pd(_mm512_add_pd(multx, multy), multz);
             __m512d denom = _mm512_mul_pd(_mm512_sqrt_pd(add), add);
-            __m512d zeros = _mm512_set1_pd(0);
-            __mmask8 cmp_res = _mm512_cmpeq_pd_mask(zeros, denom);
+            __mmask8 cmp_res = _mm512_cmp_pd_mask(zeros, denom, _CMP_EQ_OQ);
             __mmask8 notmask = _knot_mask8(cmp_res);
-            //		__m512i cmp_res = _mm512_cmpeq_epi64(_mm512_castpd_si512(zeros),_mm512_castpd_si512(denom));
-            __m512d masses = _mm512_load_pd(m+j);
-
             //		__m512d a_i = _mm512_mul_pd(_mm512_mul_pd(_mm512_set1_pd(-sim::g),masses),_mm512_rcp14_pd(denom));
-            __m512d a_i = _mm512_div_pd(_mm512_mul_pd(_mm512_set1_pd(-sim::g),masses),denom);
-            //
-            //		__m512d a_i = _mm256_mul_pd( _mm256_mul_pd(_mm256_set1_pd(-sim::g),masses),
-            //			     	_mm512_rcp14_pd(denom) );
+            //__m512d a_i = _mm512_div_pd(mg,denom);
+            __m512d a_i = _mm512_mul_pd(mg, _mm512_rcp14_pd(denom));
 
-            a_i = _mm512_maskz_andnot_pd(cmp_res, zeros, a_i);
+            a_i = _mm512_maskz_andnot_pd(notmask, zeros, a_i);
             //		a_i = _mm512_andnot_si512(_mm512_castsi512_pd(cmp_res), a_i);
 
             aix = _mm512_sub_pd(aix,_mm512_mul_pd(a_i,subx));
